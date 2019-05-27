3ad1807727527966977d1e9abc8129f3f0826d0d
asfrank <asfrank@qq.com>
2019-05-27 14:07:22 +0800

    加入demo用例

--- src/test/java/cn/futu/testCase/TestDemo.java
+++ src/test/java/cn/futu/testCase/TestDemo.java
@@ -4,10 +4,10 @@ import cn.futu.base.BaseTest;
 import cn.futu.util.Constants;
 import org.testng.annotations.Test;
 
-public class TestLogin extends BaseTest {
+public class TestDemo extends BaseTest {
 
     @Test
-    public void test_Login() throws Exception {
+    public void test_Demo() throws Exception {
         //Excel文件路径
         String excelPath = Constants.ExcelPath.FilePath;
         base_Test(excelPath);
